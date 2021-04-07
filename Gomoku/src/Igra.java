1027e1b9fb0a1cda14fd1a451149bb74101882e9
matejbolta <matej.bolta@gmail.com>
2021-04-07 21:51:13 +0200

    delo na razredu Igra

--- Gomoku/src/Igra.java
+++ Gomoku/src/Igra.java
@@ -75,4 +75,81 @@ public class Igra {
 		return rtr;
 	}
 	
+	/**
+	 * Vrne igralca, ki ima zasedeno celo vrstico (ta je podana), če ga ni, pa null.
+	 * @param v
+	 * @return WHITE/BLACK/null
+	 */
+	private Igralec lastnikVrstice(Vrstica v) {
+		int countWhite = 0;
+		int countBlack = 0;
+		
+		for (int i = 0; i < 5 && (countWhite == 0 || countBlack == 0); ++i) {
+			switch (this.plosca[v.x[i]][v.y[i]]) {
+			case WHITE: countWhite += 1; break; // Breaks switch, not for loop.
+			case BLACK: countBlack += 1; break;
+			case EMPTY: return null;
+			}
+		}
+		
+		if (countWhite == 5) return Igralec.WHITE;
+		else if (countBlack == 5) return Igralec.BLACK;
+		else return null;
+	}
+	
+	/**
+	 * Vrne zmagovalno vrsto, če je ni, pa null.
+	 * @return zmagovalnaVrstica/null
+	 */
+	public Vrstica zmagovalnaVrstica() {
+		for (Vrstica v : VRSTICE) {
+			Igralec lastnik = lastnikVrstice(v);
+			if (lastnik != null) return v;
+		}
+		return null;
+	}
+	
+	/**
+	 * Vrne trenutno stanje igre.
+	 * @return Stanje
+	 */
+	public Stanje stanjeIgre() {
+		Vrstica v = zmagovalnaVrstica();
+		
+		// if (imamo zmagovalca)
+		if (v != null) {
+			switch (plosca[v.x[0]][v.y[0]]) {
+			case WHITE: return Stanje.ZMAGA_WHITE;
+			case BLACK: return Stanje.ZMAGA_BLACK;
+			case EMPTY: assert false; // shouldn't happen
+			}
+		}
+		
+		// Zmagovalca (še) ni. Preverimo, če je na plošči še kakšno prazno polje.
+		for (int i = 0; i < N; ++i) {
+			for (int j = 0; j < N; ++j) {
+				if (this.plosca[i][j] == Polje.EMPTY) return Stanje.V_TEKU;
+			}
+		}
+		
+		// Plošča je zapolnjena.
+		return Stanje.NEODLOCENO;
+	}
+	
+	/**
+	 * Odigra potezo na koordinatah koor (igralec igra.naPotezi).
+	 * @param koor
+	 * @return true/false
+	 */
+	public boolean odigrajPotezo(Koordinate koor) {
+		// if (polje na koordinatah je prazno)
+		if (this.plosca[koor.getX()][koor.getY()] == Polje.EMPTY) {
+			this.plosca[koor.getX()][koor.getY()] = this.naPotezi.getPolje();
+			this.naPotezi = this.naPotezi.nasprotnik();
+			return true;
+		}
+		// Polje na koordinatah je že zasedeno.
+		return false;
+	}
+	
 }
